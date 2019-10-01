8ffe595ebea16ab60e986df36db6ae2acfeb7af9
G-Anonymous <44502547+tataJubjang@users.noreply.github.com>
2019-10-02 02:28:18 +0700

    Update setup-xrdp-ubuntu.sh

--- setup-xrdp-ubuntu.sh
+++ setup-xrdp-ubuntu.sh
@@ -13,15 +13,16 @@ do
 done 
 
 echo
-/bin/echo -e "\e[1;36m   !-------------------------------------------------------------!\e[0m"
-/bin/echo -e "\e[1;36m   !   Standard XRDP Installation Script  - Ver 0.5.1            !\e[0m"
-/bin/echo -e "\e[1;36m   !   Written by SCK - 1/10/2019 - check-th.tk        !\e[0m"
+/bin/echo -e "\e[1;56m   !-------------------------------------------------------------!\e[0m"
+/bin/echo -e "\e[1;36m   !   Standard XRDP Installation Script  - Ver 1.0 !\e[0m"
+/bin/echo -e "\e[1;39m   !   Written by SCK - 1/10/2019 - check-th.tk        !\e[0m"
 /bin/echo -e "\e[1;36m   !-------------------------------------------------------------!\e[0m"
 echo
 
   apt update
-  apt install xrdp
-  apt install mate-core mate-desktop-environment mate-notification-daemon
+  apt install -y xrdp
+  apt install -y ubuntu-mate-desktop
+  apt install  mate-core mate-desktop-environment mate-notification-daemon
   sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh
   systemctl enable xrdp
   systemctl start xrdp
